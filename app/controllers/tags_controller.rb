c836f9eecb732e1bb686626ff8d0d0719c05886e
alex <alexolsen03@gmail.com>
2014-04-13 19:11:22 -0500

    Sorcery authentication

--- app/controllers/tags_controller.rb
+++ app/controllers/tags_controller.rb
@@ -1,20 +1,21 @@
 class TagsController < ApplicationController
+	before_filter :require_login, only: [:destroy]
 
-def index
-	@tags = Tag.all
-end
+	def index
+		@tags = Tag.all
+	end
 
-def show
-	@tag = Tag.find(params[:id])
-end
+	def show
+		@tag = Tag.find(params[:id])
+	end
 
-def destroy
-	name = Tag.find(params[:id]).name.to_s
-	Tag.destroy(params[:id])
+	def destroy
+		name = Tag.find(params[:id]).name.to_s
+		Tag.destroy(params[:id])
 
-	flash.notice = "Tag '#{name}' removed!"
+		flash.notice = "Tag '#{name}' removed!"
 
-	redirect_to tags_path
-end
+		redirect_to tags_path
+	end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,10 @@ Blogger::Application.routes.draw do
     resources :comments
   end
   resources :tags
+  resources :authors
 
   root to: 'articles#index'
+
+  get 'login' => 'author_sessions#new'
+  get 'logout' => 'author_sessions#destroy'
 end
